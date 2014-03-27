edf82f7262726f4afc5870b96cb9744a17d5446a
briere.r@gmail.com <raph@raph.(none)>
2014-03-27 15:34:30 -0400

    faire le menage de functionexc

--- Evaluator/FunctionExc.hs
+++ Evaluator/FunctionExc.hs
@@ -1,14 +1,6 @@
 
 module Evaluator.FunctionExc (
-	applyToNative,
 	applyToDValue,	
-	convertArgsToDyn,
-	applyToDyn,
-	apply,
-	toTest,
-	convertMaybe,
-	compareArguments,
-	splitArguments,
 )where
 
 
@@ -20,17 +12,6 @@ import Data.List.Split
 import Control.Applicative
 
 
-compareFunction :: [String] -> [DValue] -> Either String [DValue]
-compareFunction s d = if ((length s) - (length d) == 0) then
-					  	compareArguments s d  else
-					  	Left "Wrong number of argument"	
-
-compareArguments :: [String] -> [DValue] -> Either String [DValue]
-compareArguments [] _ = Right []
-compareArguments _ [] = Right []
-compareArguments (s:sx) (d:dx) = (:) <$>  (compareDValueType d s) <*> (compareArguments sx dx)
-
-
 splitArguments :: String -> [String]
 splitArguments s = init $ splitOn " -> " s
 
@@ -40,37 +21,6 @@ lastArguments s = last $ splitOn " -> " s
 manyDynApp :: Dynamic -> [Dynamic] -> Dynamic
 manyDynApp f (d:[]) =   (dynApp f d)
 manyDynApp f (d:ds) = manyDynApp (dynApp f d) ds
-
-
-apply ::  Dynamic -> [Dynamic] -> DValue
-apply f a = case fromDynamic $ manyDynApp f a of
-				Just x -> DNum x
-
-applyArray ::  Dynamic -> [Dynamic] -> DValue
-applyArray f a = case fromDynamic $ manyDynApp f a of
-				Just x -> DArray $ map DNum x 
-
-
-applyToDyn :: String -> Dynamic -> [Dynamic]  ->  DValue
-applyToDyn s f a = case (s !! 0) of
-					'D' ->  apply f a
-					'[' ->  applyArray f a 
-				    
-
-applyToNative ::  (Dynamic, String) -> [DValue] -> Either String DValue
-applyToNative fx args =   (applyToDyn (lastArguments (snd fx))  $  (fst fx)) <$> (convertArgsToDyn (snd fx) args)
-					 		
-
-
-convertArgsToDyn :: String ->  [DValue] -> Either String [Dynamic]
-convertArgsToDyn f args = (convertMaybe <$>  
-							(compareFunction (splitArguments $ (f))  (args)) ) 
-
-convertMaybe :: [DValue] -> [Dynamic]
-convertMaybe d = fmap fromDValue $ d   
-
-toTest :: Dynamic -> Dynamic -> [Double]
-toTest f d = fromJust $ fromDynamic $ dynApp f d
 
 
 applyToDValue ::  (Dynamic, String) -> [DValue] -> Either String DValue
