2a31a03cdcd634f5f48af9c2a9774043b8ecf3fa
AntoineSavage <antoinesavage@hotmail.com>
2014-07-01 00:55:22 -0400

    Engine function types (no table yet)

--- src/Eval/Engine.hs
+++ src/Eval/Engine.hs
@@ -7,7 +7,7 @@ module Eval.Engine
 , tableF
 , nTimesF
 , takeF
-, sortTableF
+, sortF
 , plotLineF
 ) where
 
@@ -18,18 +18,19 @@ import Eval.Function       (Marshallable,arrayOf,table,plot,array,obj,num,(<|>))
 
 funcs :: Marshallable a => [FuncEntry a]
 funcs =   -- 1 arg functions
-        [ ("show",       ([arrayOf $ table funcs <|> plot funcs], showF))
-        , ("multi",      ([arrayOf num], multiF))
-        , ("mean",       ([arrayOf num], meanF))
-        , ("descriptive",([arrayOf num], descF))
-          -- 2 arg functions
-        , ("table",      ([array',obj'], tableF))
-        , ("nTimes",     ([num,num],    nTimesF))
-        , ("take",       ([num,array'], takeF))
-        , ("sortTable",  ([num,array'], sortTableF))
+        [ ("show",       ([arrayOf $ table' <|> plot'                                             ], showF))
+        , ("multi",      ([arrayOf num                                                            ], multiF))
+        , ("mean",       ([arrayOf num                                                            ], meanF))
+        , ("descriptive",([arrayOf num                                                            ], descF))
+          -- 2 arg functions                                                                      
+        , ("table",      ([arrayOf array'            , obj'                                       ], tableF))
+        , ("nTimes",     ([num                       , num                                        ], nTimesF))
+        , ("take",       ([num                       , table' <|> array'                          ], takeF))
+        , ("sort",       ([num                       , table funcs <|> arrayOf (array funcs)      ], sortF))
+        , ("col",        ([num                       , table funcs <|> arrayOf (array funcs)      ], colF))
           -- 3 arg functions
-        , ("plotLine",   ([array',array',obj'],plotLineF))
-        ] where array' = array funcs; obj' = obj funcs
+        , ("plotLine",   ([arrayOf num               , arrayOf num                          , obj'], plotLineF))
+        ] where table' = table funcs; plot' = plot funcs; array' = array funcs; obj' = obj funcs
 
 {-| Funcs -}
 showF      :: Func
@@ -39,16 +40,18 @@ descF      :: Func
 tableF     :: Func
 nTimesF    :: Func
 takeF      :: Func
-sortTableF :: Func
+sortF      :: Func
+colF       :: Func
 plotLineF  :: Func
 
-showF _      = error "Eval.Function::showF  [Not Implemented]"
-multiF _     = error "Eval.Function::multiF [Not Implemented]"
-meanF _      = error "Eval.Function::meanF  [Not Implemented]"
-descF _      = error "Eval.Function::descF  [Not Implemented]"
-tableF _     = error "Eval.Function::tableF     [Not Implemented]"
-nTimesF _    = error "Eval.Function::ntimesF    [Not Implemented]"
-takeF _      = error "Eval.Function::takeF      [Not Implemented]"
-sortTableF _ = error "Eval.Function::sortTableF [Not Implemented]"
-plotLineF _  = error "Eval.Function::plotLineF [Not Implemented]"
+showF _     = error "Eval.Function::showF      [Not Implemented]"
+multiF _    = error "Eval.Function::multiF     [Not Implemented]"
+meanF _     = error "Eval.Function::meanF      [Not Implemented]"
+descF _     = error "Eval.Function::descF      [Not Implemented]"
+tableF _    = error "Eval.Function::tableF     [Not Implemented]"
+nTimesF _   = error "Eval.Function::ntimesF    [Not Implemented]"
+takeF _     = error "Eval.Function::takeF      [Not Implemented]"
+sortF _     = error "Eval.Function::sortF      [Not Implemented]"
+colF _      = error "Eval.Function::colF       [Not Implemented]"
+plotLineF _ = error "Eval.Function::plotLineF  [Not Implemented]"
         
--- test/Eval/EngineTest.hs
+++ test/Eval/EngineTest.hs
@@ -3,35 +3,101 @@ module Eval.EngineTest where
 
 import Test.Framework hiding            (forAll)
                                         
-import Data.Eval                        (EvalError(..))
+import Data.Eval                        (EvalError(..),FuncEntry)
 import Data.Token                       (ExpToken(..))
 import Eval.Engine                      (funcs)
-import Eval.Function                    (arrayOf,table,plot,num,(<|>),applyFunc)
-import Eval.FunctionEvalTestUtils       (Is(..),ExpTS(..),w2,p0,applyFunc',mkFunc)
-import Parser.MonolithicParserTestUtils (P(..),ExpTA(..),un,uns)
+import Eval.Function                    (Marshallable,arrayOf,table,plot,array,obj,num,(<|>),applyFunc)
+import Eval.FunctionEvalTestUtils       (Is(..),ExpTS(..),ArrayTS(..),ObjTS(..),p0,applyFunc',mkFunc)
+import Parser.MonolithicParserTestUtils (P(..),ExpTA(..),NumTA(..),uns)
+
+fs :: Marshallable a => [FuncEntry a]
+fs = map noCall funcs where
+  noCall (n,(typeValidators,_)) = (n,(typeValidators,\_ -> error $ "Function ["++n++"] was called erroneously"))
 
 prop_NbArgs1 (P p) esTA = length esTA > 1 ==> let es = uns esTA in
-    all (\name -> Left (InvalidNbOfArgs p name 1 0)           == applyFunc' funcs p name ([] :: [ExpToken])
-               && Left (InvalidNbOfArgs p name 1 (length es)) == applyFunc' funcs p name es)
+    all (\name -> Left (InvalidNbOfArgs p name 1 0)           == applyFunc' fs p name ([] :: [ExpToken])
+               && Left (InvalidNbOfArgs p name 1 (length es)) == applyFunc' fs p name es)
       ["show","multi","mean","descriptive"]
 
 prop_NbArgs2 (P p) esTA = length esTA > 2 ==> let es = uns esTA in
-    all (\name -> Left (InvalidNbOfArgs p name 2 0)           == applyFunc' funcs p name ([] :: [ExpToken])
-               && Left (InvalidNbOfArgs p name 2 1)           == applyFunc' funcs p name (take 1 es)
-               && Left (InvalidNbOfArgs p name 2 (length es)) == applyFunc' funcs p name es)
-      ["table","nTimes","take","sortTable"]
+    all (\name -> Left (InvalidNbOfArgs p name 2 0)           == applyFunc' fs p name ([] :: [ExpToken])
+               && Left (InvalidNbOfArgs p name 2 1)           == applyFunc' fs p name (take 1 es)
+               && Left (InvalidNbOfArgs p name 2 (length es)) == applyFunc' fs p name es)
+      ["table","nTimes","take","sort","col"]
 
 prop_NbArgs3 (P p) esTA = length esTA > 3 ==> let es = uns esTA in
-  all (\name -> Left (InvalidNbOfArgs p name 3 0)           == applyFunc' funcs p name ([] :: [ExpToken])
-             && Left (InvalidNbOfArgs p name 3 1)           == applyFunc' funcs p name (take 1 es)
-             && Left (InvalidNbOfArgs p name 3 2)           == applyFunc' funcs p name (take 2 es)
-             && Left (InvalidNbOfArgs p name 3 (length es)) == applyFunc' funcs p name es)
+  all (\name -> Left (InvalidNbOfArgs p name 3 0)           == applyFunc' fs p name ([] :: [ExpToken])
+             && Left (InvalidNbOfArgs p name 3 1)           == applyFunc' fs p name (take 1 es)
+             && Left (InvalidNbOfArgs p name 3 2)           == applyFunc' fs p name (take 2 es)
+             && Left (InvalidNbOfArgs p name 3 (length es)) == applyFunc' fs p name es)
     ["plotLine"]
 
-prop_ErrorTypeShow  p ts = let es = uns ts in                       not (null es) ==> let arg = ArrayT p0 w2 es in  arrayOf (table funcs <|> plot funcs) arg == applyFunc funcs (mkFunc (un p) "show" [arg])
-prop_ErrorTypeMulti p ts = let es = uns ts in any (not.isNum) es && not (null es) ==> let arg = ArrayT p0 w2 es in  arrayOf num arg                          == applyFunc funcs (mkFunc (un p) "multi" [arg])
-prop_ErrorTypeMean  p ts = let es = uns ts in any (not.isNum) es && not (null es) ==> let arg = ArrayT p0 w2 es in  arrayOf num arg                          == applyFunc funcs (mkFunc (un p) "mean" [arg])
-prop_ErrorTypeDesc  p ts = let es = uns ts in any (not.isNum) es && not (null es) ==> let arg = ArrayT p0 w2 es in  arrayOf num arg                          == applyFunc funcs (mkFunc (un p) "descriptive" [arg])
+mk ts = let es = uns ts; arg = ArrayT p0 ("","") es in (es,arg)
+
+prop_ErrorTypeShow  (P p) w1as (ExpTS w1') = let (w1s,w1) = mk w1as in not (null w1s) && not (isArray w1') ==>  
+  arrayOf (table fs <|> plot fs) w1  == applyFunc fs (mkFunc p "show" [w1]) && 
+  arrayOf (table fs <|> plot fs) w1' == applyFunc fs (mkFunc p "show" [w1'])
+  
+prop_ErrorTypeMulti (P p) w1as (ExpTS w1') = let (w1s,w1) = mk w1as in any (not.isNum) w1s && not (isArray w1') ==>  
+  arrayOf num w1  == applyFunc fs (mkFunc p "multi" [w1]) && 
+  arrayOf num w1' == applyFunc fs (mkFunc p "multi" [w1'])
+  
+prop_ErrorTypeMean (P p) w1as (ExpTS w1') = let (w1s,w1) = mk w1as in any (not.isNum) w1s && not (isArray w1') ==>  
+  arrayOf num w1  == applyFunc fs (mkFunc p "mean" [w1]) && 
+  arrayOf num w1' == applyFunc fs (mkFunc p "mean" [w1'])
+  
+prop_ErrorTypeDesc (P p) w1as (ExpTS w1') = let (w1s,w1) = mk w1as in any (not.isNum) w1s && not (isArray w1') ==>  
+  arrayOf num w1  == applyFunc fs (mkFunc p "descriptive" [w1]) && 
+  arrayOf num w1' == applyFunc fs (mkFunc p "descriptive" [w1'])
+
+prop_ErrorTypeTable (P p) g1ass (ObjTS g2) w1ass (ExpTS w1') (ExpTS w2) = 
+  let (_,g1) = mk g1ass; (w1ss,w1) = mk w1ass; in any (not.isArray) w1ss && not (isArray w1') && not (isObj w2) ==>
+    arrayOf (array fs) w1  == applyFunc fs (mkFunc p "table" [w1 ,g2]) &&
+    arrayOf (array fs) w1' == applyFunc fs (mkFunc p "table" [w1',w2]) &&
+    obj fs w2              == applyFunc fs (mkFunc p "table" [g1 ,w2])
+
+prop_ErrorTypeNTimes (P p) (NumTA _ g1) (NumTA _ g2) (ExpTS w1) (ExpTS w2) = not (isNum w1) && not (isNum w2) ==>
+  num w1 == applyFunc fs (mkFunc p "nTimes" [w1,g2]) &&
+  num w1 == applyFunc fs (mkFunc p "nTimes" [w1,w2]) &&
+  num w2 == applyFunc fs (mkFunc p "nTimes" [g1,w2]) 
+
+prop_ErrorTypeTake (P p) (NumTA _ g1) (ArrayTS g2) (ExpTS w1) (ExpTS w2) = not (isNum w1) && not (isArray w2) ==>
+   num w1                    == applyFunc fs (mkFunc p "take" [w1,g2]) &&
+   num w1                    == applyFunc fs (mkFunc p "take" [w1,w2]) &&
+  (table fs <|> array fs) w2 == applyFunc fs (mkFunc p "take" [g1,w2])
+
+prop_ErrorTypeSort (P p) (NumTA _ g1) g2as (ExpTS w1) w2as (ExpTS w2') = 
+  let (_,g2) = mk g2as; (w2s,w2) = mk w2as in not (isNum w1) && any (not.isArray) w2s && not (isArray w2') ==>
+     num w1                               == applyFunc fs (mkFunc p "sort" [w1,g2]) &&
+     num w1                               == applyFunc fs (mkFunc p "sort" [w1,w2]) &&
+     num w1                               == applyFunc fs (mkFunc p "sort" [w1,w2']) &&
+    (table fs <|> arrayOf (array fs)) w2  == applyFunc fs (mkFunc p "sort" [g1,w2]) &&
+    (table fs <|> arrayOf (array fs)) w2' == applyFunc fs (mkFunc p "sort" [g1,w2'])
+
+prop_ErrorTypeCol (P p) (NumTA _ g1) g2ass (ExpTS w1) w2as (ExpTS w2') = 
+  let (_,g2) = mk g2ass; (w2s,w2) = mk w2as in not (isNum w1) && any (not.isArray) w2s && not (isArray w2') ==>
+     num w1                               == applyFunc fs (mkFunc p "col" [w1,g2]) &&
+     num w1                               == applyFunc fs (mkFunc p "col" [w1,w2]) &&
+     num w1                               == applyFunc fs (mkFunc p "col" [w1,w2']) &&
+    (table fs <|> arrayOf (array fs)) w2  == applyFunc fs (mkFunc p "col" [g1,w2]) &&
+    (table fs <|> arrayOf (array fs)) w2' == applyFunc fs (mkFunc p "col" [g1,w2'])
+
+prop_ErrorTypePlotLine (P p) g1as g2as (ObjTS g3) w1as (ExpTS w1') w2as (ExpTS w2') (ExpTS w3) = 
+  let (_,g1) = mk g1as; (_,g2) = mk g2as; (w1s,w1) = mk w1as; (w2s,w2) = mk w2as 
+  in any (not.isNum) w1s && not (isArray w1') && any (not.isNum) w2s && not (isArray w2') && not (isObj w3) ==>
+    arrayOf num w1  == applyFunc fs (mkFunc p "plotLine" [w1 ,g2 ,g3]) &&
+    arrayOf num w1  == applyFunc fs (mkFunc p "plotLine" [w1 ,w2 ,g3]) &&
+    arrayOf num w1  == applyFunc fs (mkFunc p "plotLine" [w1 ,w2',g3]) &&
+    arrayOf num w1  == applyFunc fs (mkFunc p "plotLine" [w1 ,g2 ,w3]) &&
+    arrayOf num w1' == applyFunc fs (mkFunc p "plotLine" [w1',g2 ,g3]) &&
+    arrayOf num w1' == applyFunc fs (mkFunc p "plotLine" [w1',w2 ,g3]) &&
+    arrayOf num w1' == applyFunc fs (mkFunc p "plotLine" [w1',w2',g3]) &&
+    arrayOf num w1' == applyFunc fs (mkFunc p "plotLine" [w1',g2 ,w3]) &&
+    arrayOf num w2  == applyFunc fs (mkFunc p "plotLine" [g1 ,w2 ,g3]) &&
+    arrayOf num w2  == applyFunc fs (mkFunc p "plotLine" [g1 ,w2 ,w3]) &&
+    arrayOf num w2' == applyFunc fs (mkFunc p "plotLine" [g1 ,w2',g3]) &&
+    arrayOf num w2' == applyFunc fs (mkFunc p "plotLine" [g1 ,w2',w3]) &&
+    obj fs w3       == applyFunc fs (mkFunc p "plotLine" [g1 ,g2 ,w3])
 
 {-| Mandatory type signatures -}
 type NbArgTest = P -> [ExpTA] ->  Property
@@ -39,10 +105,18 @@ prop_NbArgs1 :: NbArgTest
 prop_NbArgs2 :: NbArgTest 
 prop_NbArgs3 :: NbArgTest 
 
-prop_ErrorTypeShow  :: P -> [ExpTS] -> Property
-prop_ErrorTypeMulti :: P -> [ExpTS] -> Property
-prop_ErrorTypeMean  :: P -> [ExpTS] -> Property
-prop_ErrorTypeDesc  :: P -> [ExpTS] -> Property
+prop_ErrorTypeShow  :: P -> [ExpTS] -> ExpTS -> Property
+prop_ErrorTypeMulti :: P -> [ExpTS] -> ExpTS -> Property
+prop_ErrorTypeMean  :: P -> [ExpTS] -> ExpTS -> Property
+prop_ErrorTypeDesc  :: P -> [ExpTS] -> ExpTS -> Property
+
+prop_ErrorTypeTable  :: P -> [ArrayTS] ->   ObjTS   -> [ExpTS] ->  ExpTS  -> ExpTS -> Property
+prop_ErrorTypeNTimes :: P ->  NumTA    ->   NumTA   ->  ExpTS  ->  ExpTS           -> Property
+prop_ErrorTypeTake   :: P ->  NumTA    ->   ArrayTS ->  ExpTS  ->  ExpTS           -> Property
+prop_ErrorTypeSort   :: P ->  NumTA    -> [ArrayTS] ->  ExpTS  -> [ExpTS] -> ExpTS -> Property
+prop_ErrorTypeCol    :: P ->  NumTA    -> [ArrayTS] ->  ExpTS  -> [ExpTS] -> ExpTS -> Property
+
+prop_ErrorTypePlotLine :: P -> [NumTA] -> [NumTA]   -> ObjTS   -> [ExpTS] -> ExpTS -> [ExpTS] -> ExpTS -> ExpTS -> Property
 
 {-|
 unlines $  [ "show = show([tservice,tsalaire,trente])" , "tservice = table([[service]],{col:[\"service\"]})" , "tsalaire = table([salaires],{col:[\"salaire\"]})" , "trente = table([rente],{col:[\"rente\"]})" , "rente = multi([0.02,moyensalaire,service])" , "moyensalaire = mean(salaires)" , "salaires = [55000,60000,45000]" , "service = 35" ]
