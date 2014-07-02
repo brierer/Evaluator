51998e335024c658c04a30658b1f707e970e5484
AntoineSavage <antoinesavage@hotmail.com>
2014-07-02 13:59:52 -0400

    Gonna change the functgion evaluation monad to a state/either stack from a either stack with explicit state ([FuncEntry])

--- test/Eval/EngineTest.hs
+++ test/Eval/EngineTest.hs
@@ -1,111 +1,139 @@
 {-# OPTIONS_GHC -F -pgmF htfpp #-}
 module Eval.EngineTest where
 
-import Test.Framework hiding            (forAll)
-                                        
-import Data.Eval                        (EvalError(..))
-import Data.Token                       (ExpToken(..))
-import Eval.EngineTestUtils             (fs,addFunc,mk)
-import Eval.Function                    (Marshallable,arrayOf,table,plot,array,obj,num,(<|>),applyFunc)
-import Eval.FunctionEvalTestUtils       (Is(..),TableOA(..),ExpTS(..),ArrayTS(..),ObjTS(..),applyFunc',mkFunc)
-import Parser.MonolithicParserTestUtils (P(..),ExpTA(..),NumTA(..),uns)
+import Test.Framework hiding               (forAll)
+import qualified Eval.EngineTestUtils as E (fs)
+
+import Data.Eval                           (EvalError(..),ExpObj(..))
+import Data.Token                          (ExpToken(..))
+import Eval.EngineTestUtils                (addFunc,mk,oneArrayOfNum,success)
+import Eval.Function                       (Marshallable,arrayOf,table,plot,array,obj,num,(<|>),applyFunc)
+import Eval.FunctionEvalTestUtils          (Is(..),ExpOA(..),TableOA(..),ExpTS(..),ArrayTS(..),ObjTS(..),applyFunc',mkFunc)
+import Parser.MonolithicParserTestUtils    (P(..),ExpTA(..),NumTA(..),uns)
 
 prop_NbArgs1 (P p) esTA = length esTA > 1 ==> let es = uns esTA in
-    all (\name -> Left (InvalidNbOfArgs p name 1 0)           == applyFunc' fs p name ([] :: [ExpToken])
-               && Left (InvalidNbOfArgs p name 1 (length es)) == applyFunc' fs p name es)
+    all (\name -> Left (InvalidNbOfArgs p name 1 0)           == applyFunc' E.fs p name ([] :: [ExpToken])
+               && Left (InvalidNbOfArgs p name 1 (length es)) == applyFunc' E.fs p name es)
       ["show","multi","mean","descriptive"]
 
 prop_NbArgs2 (P p) esTA = length esTA > 2 ==> let es = uns esTA in
-    all (\name -> Left (InvalidNbOfArgs p name 2 0)           == applyFunc' fs p name ([] :: [ExpToken])
-               && Left (InvalidNbOfArgs p name 2 1)           == applyFunc' fs p name (take 1 es)
-               && Left (InvalidNbOfArgs p name 2 (length es)) == applyFunc' fs p name es)
+    all (\name -> Left (InvalidNbOfArgs p name 2 0)           == applyFunc' E.fs p name ([] :: [ExpToken])
+               && Left (InvalidNbOfArgs p name 2 1)           == applyFunc' E.fs p name (take 1 es)
+               && Left (InvalidNbOfArgs p name 2 (length es)) == applyFunc' E.fs p name es)
       ["table","nTimes","take","sort","col"]
 
 prop_NbArgs3 (P p) esTA = length esTA > 3 ==> let es = uns esTA in
-  all (\name -> Left (InvalidNbOfArgs p name 3 0)           == applyFunc' fs p name ([] :: [ExpToken])
-             && Left (InvalidNbOfArgs p name 3 1)           == applyFunc' fs p name (take 1 es)
-             && Left (InvalidNbOfArgs p name 3 2)           == applyFunc' fs p name (take 2 es)
-             && Left (InvalidNbOfArgs p name 3 (length es)) == applyFunc' fs p name es)
+  all (\name -> Left (InvalidNbOfArgs p name 3 0)           == applyFunc' E.fs p name ([] :: [ExpToken])
+             && Left (InvalidNbOfArgs p name 3 1)           == applyFunc' E.fs p name (take 1 es)
+             && Left (InvalidNbOfArgs p name 3 2)           == applyFunc' E.fs p name (take 2 es)
+             && Left (InvalidNbOfArgs p name 3 (length es)) == applyFunc' E.fs p name es)
     ["plotLine"]
 
-prop_TypeErrorShow  (P p) w1as (ExpTS w1') = let (w1s,w1) = mk w1as in not (null w1s) && not (isArray w1') ==>  
-  arrayOf (table fs <|> plot fs) w1  == applyFunc fs (mkFunc p "show" [w1]) && 
-  arrayOf (table fs <|> plot fs) w1' == applyFunc fs (mkFunc p "show" [w1'])
-  
-prop_TypeErrorMulti (P p) w1as (ExpTS w1') = let (w1s,w1) = mk w1as in any (not.isNum) w1s && not (isArray w1') ==>  
-  arrayOf num w1  == applyFunc fs (mkFunc p "multi" [w1]) && 
-  arrayOf num w1' == applyFunc fs (mkFunc p "multi" [w1'])
-  
-prop_TypeErrorMean (P p) w1as (ExpTS w1') = let (w1s,w1) = mk w1as in any (not.isNum) w1s && not (isArray w1') ==>  
-  arrayOf num w1  == applyFunc fs (mkFunc p "mean" [w1]) && 
-  arrayOf num w1' == applyFunc fs (mkFunc p "mean" [w1'])
-  
-prop_TypeErrorDesc (P p) w1as (ExpTS w1') = let (w1s,w1) = mk w1as in any (not.isNum) w1s && not (isArray w1') ==>  
-  arrayOf num w1  == applyFunc fs (mkFunc p "descriptive" [w1]) && 
-  arrayOf num w1' == applyFunc fs (mkFunc p "descriptive" [w1'])
+prop_TypeErrorShow  (P p) (ExpOA g1r) w1as (ExpTS w1') =
+  let (fs,g1) = addFunc "tableOrPlot" g1r; (w1s,w1) = mk w1as in (isTable g1r || isPlot g1r) && not (null w1s) && not (isArray w1') ==>
+    arrayOf (table fs <|> plot fs) w1  == applyFunc fs (mkFunc p "show" [w1]) &&
+    arrayOf (table fs <|> plot fs) w1' == applyFunc fs (mkFunc p "show" [w1']) 
+--    &&
+--    success "show"                     == applyFunc fs (mkFunc p "show" [g1])
 
-prop_TypeErrorTable (P p) g1ass (ObjTS g2) w1ass (ExpTS w1') (ExpTS w2) = 
+prop_TypeErrorMulti (P p) g1ras w1as (ExpTS w1') =
+  let (fs,g1,w1s,w1) = oneArrayOfNum g1ras w1as in any (not.isNum) w1s && not (isArray w1') ==>
+    arrayOf num w1  == applyFunc fs (mkFunc p "multi" [w1]) &&
+    arrayOf num w1' == applyFunc fs (mkFunc p "multi" [w1']) 
+--    &&
+--    success "multi" == applyFunc fs (mkFunc p "multi" [g1])
+
+prop_TypeErrorMean (P p) g1ras w1as (ExpTS w1') =
+  let (fs,g1,w1s,w1) = oneArrayOfNum g1ras w1as in any (not.isNum) w1s && not (isArray w1') ==>
+    arrayOf num w1  == applyFunc fs (mkFunc p "mean" [w1]) &&
+    arrayOf num w1' == applyFunc fs (mkFunc p "mean" [w1']) 
+--    &&
+--    success "mean"  == applyFunc fs (mkFunc p "mean" [g1])
+
+prop_TypeErrorDesc (P p) g1ras w1as (ExpTS w1') =
+  let (fs,g1,w1s,w1) = oneArrayOfNum g1ras w1as in any (not.isNum) w1s && not (isArray w1') ==>
+    arrayOf num w1        == applyFunc fs (mkFunc p "descriptive" [w1]) &&
+    arrayOf num w1'       == applyFunc fs (mkFunc p "descriptive" [w1'])
+--     &&
+--    success "descriptive" == applyFunc fs (mkFunc p "descriptive" [g1])
+
+prop_TypeErrorTable (P p) g1ass (ObjTS g2) w1ass (ExpTS w1') (ExpTS w2) =
   let (_,g1) = mk g1ass; (w1ss,w1) = mk w1ass; in any (not.isArray) w1ss && not (isArray w1') && not (isObj w2) ==>
-    arrayOf (array fs) w1  == applyFunc fs (mkFunc p "table" [w1 ,g2]) &&
-    arrayOf (array fs) w1' == applyFunc fs (mkFunc p "table" [w1',w2]) &&
-    obj fs w2              == applyFunc fs (mkFunc p "table" [g1 ,w2])
+    arrayOf (array E.fs) w1  == applyFunc E.fs (mkFunc p "table" [w1 ,g2]) &&
+    arrayOf (array E.fs) w1' == applyFunc E.fs (mkFunc p "table" [w1',w2]) &&
+    obj E.fs w2              == applyFunc E.fs (mkFunc p "table" [g1 ,w2]) 
+--    &&
+--    success "table"          == applyFunc E.fs (mkFunc p "table" [g1 ,g2])
 
 prop_TypeErrorNTimes (P p) (NumTA _ g1) (NumTA _ g2) (ExpTS w1) (ExpTS w2) = not (isNum w1) && not (isNum w2) ==>
-  num w1 == applyFunc fs (mkFunc p "nTimes" [w1,g2]) &&
-  num w1 == applyFunc fs (mkFunc p "nTimes" [w1,w2]) &&
-  num w2 == applyFunc fs (mkFunc p "nTimes" [g1,w2]) 
+  num w1           == applyFunc E.fs (mkFunc p "nTimes" [w1,g2]) &&
+  num w1           == applyFunc E.fs (mkFunc p "nTimes" [w1,w2]) &&
+  num w2           == applyFunc E.fs (mkFunc p "nTimes" [g1,w2]) 
+--  &&
+--  success "nTimes" == applyFunc E.fs (mkFunc p "nTimes" [g1,g2])
 
 prop_TypeErrorTake (P p) (NumTA _ g1) (ArrayTS g2) (TableOA g2r) (ExpTS w1) (ExpTS w2) = not (isNum w1) && not (isArray w2) ==>
-  let (fs',g2') = addFunc fs "testFuncTable" g2r in 
-   num w1                      == applyFunc fs' (mkFunc p "take" [w1,g2])  &&
-   num w1                      == applyFunc fs' (mkFunc p "take" [w1,g2']) &&
-   num w1                      == applyFunc fs' (mkFunc p "take" [w1,w2])  &&
-  (table fs' <|> array fs') w2 == applyFunc fs' (mkFunc p "take" [g1,w2])
+  let (fs,g2') = addFunc "table" g2r in
+   num w1                    == applyFunc fs (mkFunc p "take" [w1,g2])  &&
+   num w1                    == applyFunc fs (mkFunc p "take" [w1,g2']) &&
+   num w1                    == applyFunc fs (mkFunc p "take" [w1,w2])  &&
+  (table fs <|> array fs) w2 == applyFunc fs (mkFunc p "take" [g1,w2]) &&
+   success "take"            == applyFunc fs (mkFunc p "take" [g1,g2]) 
+--   &&
+--   success "take"            == applyFunc fs (mkFunc p "take" [g1,g2'])
 
-prop_TypeErrorSort (P p) (NumTA _ g1) g2as (TableOA g2r) (ExpTS w1) w2as (ExpTS w2') = 
-  let (_,g2) = mk g2as; (w2s,w2) = mk w2as; (fs',g2') = addFunc fs "testFuncTable" g2r in not (isNum w1) && any (not.isArray) w2s && not (isArray w2') ==>
-     num w1                                == applyFunc fs' (mkFunc p "sort" [w1,g2]) &&
-     num w1                                == applyFunc fs' (mkFunc p "sort" [w1,g2']) &&
-     num w1                                == applyFunc fs' (mkFunc p "sort" [w1,w2]) &&
-     num w1                                == applyFunc fs' (mkFunc p "sort" [w1,w2']) &&
-    (table fs' <|> arrayOf (array fs)) w2  == applyFunc fs' (mkFunc p "sort" [g1,w2]) &&
-    (table fs' <|> arrayOf (array fs)) w2' == applyFunc fs' (mkFunc p "sort" [g1,w2'])
+prop_TypeErrorSort (P p) (NumTA _ g1) g2as (TableOA g2r) (ExpTS w1) w2as (ExpTS w2') =
+  let (_,g2) = mk g2as; (w2s,w2) = mk w2as; (fs,g2') = addFunc "table" g2r in not (isNum w1) && any (not.isArray) w2s && not (isArray w2') ==>
+     num w1                               == applyFunc fs (mkFunc p "sort" [w1,g2]) &&
+     num w1                               == applyFunc fs (mkFunc p "sort" [w1,g2']) &&
+     num w1                               == applyFunc fs (mkFunc p "sort" [w1,w2]) &&
+     num w1                               == applyFunc fs (mkFunc p "sort" [w1,w2']) &&
+    (table fs <|> arrayOf (array fs)) w2  == applyFunc fs (mkFunc p "sort" [g1,w2]) &&
+    (table fs <|> arrayOf (array fs)) w2' == applyFunc fs (mkFunc p "sort" [g1,w2']) 
+--    &&
+--     success "sort"                       == applyFunc fs (mkFunc p "sort" [g1,g2]) &&
+--     success "sort"                       == applyFunc fs (mkFunc p "sort" [g1,g2'])
 
-prop_TypeErrorCol (P p) (NumTA _ g1) g2ass (TableOA g2r) (ExpTS w1) w2as (ExpTS w2') = 
-  let (_,g2) = mk g2ass; (w2s,w2) = mk w2as; (fs',g2') = addFunc fs "testFuncTable" g2r in not (isNum w1) && any (not.isArray) w2s && not (isArray w2') ==>
-     num w1                                 == applyFunc fs (mkFunc p "col" [w1,g2]) &&
-     num w1                                 == applyFunc fs (mkFunc p "col" [w1,g2']) &&
-     num w1                                 == applyFunc fs (mkFunc p "col" [w1,w2]) &&
-     num w1                                 == applyFunc fs (mkFunc p "col" [w1,w2']) &&
-    (table fs' <|> arrayOf (array fs')) w2  == applyFunc fs (mkFunc p "col" [g1,w2]) &&
-    (table fs' <|> arrayOf (array fs')) w2' == applyFunc fs (mkFunc p "col" [g1,w2'])
+prop_TypeErrorCol (P p) (NumTA _ g1) g2ass (TableOA g2r) (ExpTS w1) w2as (ExpTS w2') =
+  let (_,g2) = mk g2ass; (w2s,w2) = mk w2as; (fs,g2') = addFunc "table" g2r in not (isNum w1) && any (not.isArray) w2s && not (isArray w2') ==>
+     num w1                               == applyFunc fs (mkFunc p "col" [w1,g2]) &&
+     num w1                               == applyFunc fs (mkFunc p "col" [w1,g2']) &&
+     num w1                               == applyFunc fs (mkFunc p "col" [w1,w2]) &&
+     num w1                               == applyFunc fs (mkFunc p "col" [w1,w2']) &&
+    (table fs <|> arrayOf (array fs)) w2  == applyFunc fs (mkFunc p "col" [g1,w2]) &&
+    (table fs <|> arrayOf (array fs)) w2' == applyFunc fs (mkFunc p "col" [g1,w2']) 
+--    &&
+--     success "col"                        == applyFunc fs (mkFunc p "col" [g1,g2]) &&
+--     success "col"                        == applyFunc fs (mkFunc p "col" [g1,g2'])
 
-prop_TypeErrorPlotLine (P p) g1as g2as (ObjTS g3) w1as (ExpTS w1') w2as (ExpTS w2') (ExpTS w3) = 
-  let (_,g1) = mk g1as; (_,g2) = mk g2as; (w1s,w1) = mk w1as; (w2s,w2) = mk w2as 
+prop_TypeErrorPlotLine (P p) g1as g2as (ObjTS g3) w1as (ExpTS w1') w2as (ExpTS w2') (ExpTS w3) =
+  let (_,g1) = mk g1as; (_,g2) = mk g2as; (w1s,w1) = mk w1as; (w2s,w2) = mk w2as
   in any (not.isNum) w1s && not (isArray w1') && any (not.isNum) w2s && not (isArray w2') && not (isObj w3) ==>
-    arrayOf num w1  == applyFunc fs (mkFunc p "plotLine" [w1 ,g2 ,g3]) &&
-    arrayOf num w1  == applyFunc fs (mkFunc p "plotLine" [w1 ,w2 ,g3]) &&
-    arrayOf num w1  == applyFunc fs (mkFunc p "plotLine" [w1 ,w2',g3]) &&
-    arrayOf num w1  == applyFunc fs (mkFunc p "plotLine" [w1 ,g2 ,w3]) &&
-    arrayOf num w1' == applyFunc fs (mkFunc p "plotLine" [w1',g2 ,g3]) &&
-    arrayOf num w1' == applyFunc fs (mkFunc p "plotLine" [w1',w2 ,g3]) &&
-    arrayOf num w1' == applyFunc fs (mkFunc p "plotLine" [w1',w2',g3]) &&
-    arrayOf num w1' == applyFunc fs (mkFunc p "plotLine" [w1',g2 ,w3]) &&
-    arrayOf num w2  == applyFunc fs (mkFunc p "plotLine" [g1 ,w2 ,g3]) &&
-    arrayOf num w2  == applyFunc fs (mkFunc p "plotLine" [g1 ,w2 ,w3]) &&
-    arrayOf num w2' == applyFunc fs (mkFunc p "plotLine" [g1 ,w2',g3]) &&
-    arrayOf num w2' == applyFunc fs (mkFunc p "plotLine" [g1 ,w2',w3]) &&
-    obj fs w3       == applyFunc fs (mkFunc p "plotLine" [g1 ,g2 ,w3])
+    arrayOf num w1     == applyFunc E.fs (mkFunc p "plotLine" [w1 ,g2 ,g3]) &&
+    arrayOf num w1     == applyFunc E.fs (mkFunc p "plotLine" [w1 ,w2 ,g3]) &&
+    arrayOf num w1     == applyFunc E.fs (mkFunc p "plotLine" [w1 ,w2',g3]) &&
+    arrayOf num w1     == applyFunc E.fs (mkFunc p "plotLine" [w1 ,g2 ,w3]) &&
+    arrayOf num w1'    == applyFunc E.fs (mkFunc p "plotLine" [w1',g2 ,g3]) &&
+    arrayOf num w1'    == applyFunc E.fs (mkFunc p "plotLine" [w1',w2 ,g3]) &&
+    arrayOf num w1'    == applyFunc E.fs (mkFunc p "plotLine" [w1',w2',g3]) &&
+    arrayOf num w1'    == applyFunc E.fs (mkFunc p "plotLine" [w1',g2 ,w3]) &&
+    arrayOf num w2     == applyFunc E.fs (mkFunc p "plotLine" [g1 ,w2 ,g3]) &&
+    arrayOf num w2     == applyFunc E.fs (mkFunc p "plotLine" [g1 ,w2 ,w3]) &&
+    arrayOf num w2'    == applyFunc E.fs (mkFunc p "plotLine" [g1 ,w2',g3]) &&
+    arrayOf num w2'    == applyFunc E.fs (mkFunc p "plotLine" [g1 ,w2',w3]) &&
+    obj E.fs w3        == applyFunc E.fs (mkFunc p "plotLine" [g1 ,g2 ,w3]) 
+--    &&
+--    success "plotLine" == applyFunc E.fs (mkFunc p "plotLine" [g1 ,g2 ,g3])
 
 {-| Mandatory type signatures -}
-prop_NbArgs1 :: P -> [ExpTA] ->  Property 
-prop_NbArgs2 :: P -> [ExpTA] ->  Property 
-prop_NbArgs3 :: P -> [ExpTA] ->  Property 
+prop_NbArgs1 :: P -> [ExpTA] ->  Property
+prop_NbArgs2 :: P -> [ExpTA] ->  Property
+prop_NbArgs3 :: P -> [ExpTA] ->  Property
 
-prop_TypeErrorShow  :: P -> [ExpTS] -> ExpTS -> Property
-prop_TypeErrorMulti :: P -> [ExpTS] -> ExpTS -> Property
-prop_TypeErrorMean  :: P -> [ExpTS] -> ExpTS -> Property
-prop_TypeErrorDesc  :: P -> [ExpTS] -> ExpTS -> Property
+prop_TypeErrorShow  :: P ->  ExpOA  -> [ExpTS] -> ExpTS -> Property
+prop_TypeErrorMulti :: P -> [ExpOA] -> [ExpTS] -> ExpTS  -> Property
+prop_TypeErrorMean  :: P -> [ExpOA] -> [ExpTS] -> ExpTS  -> Property
+prop_TypeErrorDesc  :: P -> [ExpOA] -> [ExpTS] -> ExpTS  -> Property
 
 prop_TypeErrorTable  :: P -> [ArrayTS] ->   ObjTS   -> [ExpTS] ->  ExpTS  ->  ExpTS           -> Property
 prop_TypeErrorNTimes :: P ->  NumTA    ->   NumTA   ->  ExpTS  ->  ExpTS                      -> Property
--- test/Eval/EngineTestUtils.hs
+++ test/Eval/EngineTestUtils.hs
@@ -1,16 +1,19 @@
 module Eval.EngineTestUtils where
 
-import Data.Eval                        (FuncEntry)
+import Data.Eval                        (ExpObj(..),FuncEntry)
 import Data.Token                       (IdToken(..),ExpToken(..))
 import Eval.Engine                      (funcs)
 import Eval.Function                    (Marshallable)
-import Eval.FunctionEvalTestUtils       (p0)
+import Eval.FunctionEvalTestUtils       (p0,w2)
 import Parser.MonolithicParserTestUtils (uns)
 
 fs :: Marshallable a => [FuncEntry a]
-fs = map noCall funcs where
-  noCall (n,(typeValidators,_)) = (n,(typeValidators,\_ -> error $ "Function ["++n++"] was called erroneously"))
+fs = map noCall funcs where noCall (n,(typeValidators,_)) = (n,(typeValidators,\_ -> success n))
 
-mk ts = let es = uns ts; arg = ArrayT p0 ("","") es in (es,arg)
-addFunc xs fn r = let xs' = (fn,([],\_ -> return r)):xs; g = FuncT "" (IdT p0 ("","") fn) [] in (xs',g)
+mk  ts = let es = uns ts; arg = ArrayT p0 w2 es in (es,arg)
 
+oneArrayOfNum g1ras w1as = let es = uns g1ras; arg = ArrayO p0 es; (xs,g1) = addFunc "arrayOfNum" arg; (w1s,w1) = mk w1as in (xs,g1,w1s,w1)
+
+addFunc fn r = let xs' = (fn,([],\_ -> return r)):fs; g = FuncT "" (IdT p0 ("","") fn) [] in (xs',g)
+
+success n = Right $ StrO p0 $ "Mocked Success ["++n++"]"
