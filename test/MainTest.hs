bb950dc134d6dace34dc8733ba0ea5b8467f0d90
AntoineSavage <antoinesavage@hotmail.com>
2014-07-30 14:02:34 -0400

    Reorder tests

--- test/MainTest.hs
+++ test/MainTest.hs
@@ -6,19 +6,19 @@ import {-@ HTF_TESTS @-} Engine.EngineUnitFailureType
 import {-@ HTF_TESTS @-} Engine.EngineUnitFailureConstraint
 import {-@ HTF_TESTS @-} Engine.EngineUnitSuccess
 import {-@ HTF_TESTS @-} Marshall.MarshallUnit
-import {-@ HTF_TESTS @-} MultiPass.MultiPassUnit
-import {-@ HTF_TESTS @-} Parser.ParserUnit
 import {-@ HTF_TESTS @-} MatchType.MatchTypeUnitFailure
 import {-@ HTF_TESTS @-} MatchType.MatchTypeUnitSuccess
+import {-@ HTF_TESTS @-} MultiPass.MultiPassUnit
+import {-@ HTF_TESTS @-} Parser.ParserUnit
 
 import {-@ HTF_TESTS @-} Engine.EnginePropFailureType
 import {-@ HTF_TESTS @-} Engine.EnginePropFailureConstraint
 import {-@ HTF_TESTS @-} Engine.EnginePropSuccess
 import {-@ HTF_TESTS @-} Marshall.MarshallPropFailure
 import {-@ HTF_TESTS @-} Marshall.MarshallPropSuccess
+import {-@ HTF_TESTS @-} MatchType.MatchTypeProp
 import {-@ HTF_TESTS @-} MultiPass.MultiPassProp
 import {-@ HTF_TESTS @-} Parser.ParserProp
-import {-@ HTF_TESTS @-} MatchType.MatchTypeProp
 
 main :: IO ()
 main = htfMain htf_importedTests
